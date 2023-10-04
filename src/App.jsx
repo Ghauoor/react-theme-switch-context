284e0a2e91a53a23a7948f85e84ea516690488be
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-10-04 16:09:12 +0500

    Theme Switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import Login from "./screens/Login";
-import Profile from "./screens/Profile";
-import UserContextProvider from "./context/UserContextProvider";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //change the theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <UserContextProvider>
-      <Login />
-      <Profile />
-    </UserContextProvider>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme Button */}
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
